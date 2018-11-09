e01ee225be8cd9a854b0054257adbba8ec8580f4
Nenetti <akinori1090@gmail.com>
2018-11-09 13:43:18 +0900

    インストーラー恒心

--- sound/install.sh
+++ sound/install.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
+#svox pico インストーラー
 sudo apt-get install -y libttspico-utils
+#openJtalkインストーラー
 sudo apt-get install -y open-jtalk open-jtalk-mecab-naist-jdic hts-voice-nitech-jp-atr503-m001
+
 
